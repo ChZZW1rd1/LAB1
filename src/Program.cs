ec86deb76b9b3cabbeef60c4130b2c1eb992b69e
ChZZW1rd1 <95560833+ChZZW1rd1@users.noreply.github.com>
2023-02-26 23:41:22 +0700

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person MyPerson1 = new person();
+            MyPerson1.NameSurname = ("Бжежгевич Бегброн");
+            MyPerson1.Gender = (0);
+            MyPerson1.Age = (27);
+            MyPerson1.Print();
+            //person MyPerson3 = new person();
+            // MyPerson3.Input();
+            //MyPerson3.Print();
+
+            person MyPerson2 = new person("Вжегач Алексей Павлович", 0, 31);
+
+            MyPerson2.Print();
+            Organization Yandex = new Organization("Yandex", 1997, MyPerson2);
+            Yandex.Print();
+
+            Worker worker1 = new Worker("Григоренко Иван Рашевич", 0, 25);
+            worker1.Print();
+
+            HR.Employ(worker1, Yandex, 150000, "Программист");
+            worker1.Print();
+            HR.Dismiss(worker1);
+            worker1.Print();
+        }
+    }
+}
